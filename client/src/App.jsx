ac53e306321ae0607ad5b500ac9a0602770747ee
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-08-22 14:24:03 +0530

    created the simple react app and added some basic pags of the website and used the react router dom for the routing purpose and completed it successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+
+import SignUp from './Pages/SignUp'
 
 function App() {
   return (
-    <div className='text-red-500'>Appasa</div>
+    <div >
+
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/' element={<App></App>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/profile' element={<Profile></Profile>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
+    
+    
+    </div>
   )
 }
 
