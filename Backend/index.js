9209a6e737066f7bc1e7485d53ded2b8d47c2a0b
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-08-23 11:24:34 +0530

    created a branch called databBase and used the mongoose library to to connect to the monodb data base and completed succssfully

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,26 @@
 import express from "express"
+import mongoose from "mongoose";
 
+import dotenv from "dotenv"
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+
+  console.log('DataBase connected successfully');
+}
+  
+).catch((err)=>{
+  console.log('error', err);
+  
+})
 
 
 const app = express()
 
 app.listen(3000 ,()=>{
-  console.log('Server is running on Port 3000 !!');
+  console.log('Server is running on Port 3000 ! !');
   
 })
+ 
